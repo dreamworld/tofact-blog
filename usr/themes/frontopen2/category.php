67f30fcfc753801861a2e7fd29df4d7801317214
Kang Liu <Kang.Liu@tum.de>
2014-04-04 22:16:47 +0200

    deleted a custom theme

--- usr/themes/frontopen2/category.php
+++
@@ -1,8 +0,0 @@
-<?php
-if ($this->categories[0]['mid'] == $this->options->picCat)
-{
-//  $archive->parameter->pageSize = 20;
-$this->need('loop-categorypic.php');
-die;}
-$this->need('loop-category.php');
-?>
